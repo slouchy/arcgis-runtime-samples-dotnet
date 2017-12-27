cf278aa6d9b894345cc3665d292d8e1e779869b0
Nathan Castle <ncastle@esri.com>
2017-12-27 12:18:26 -0800

    create attribtues windows

---
+++ src/ArcGISRuntime.Samples.Shared/Attributes/LayoutFilesAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcGISRuntime.Samples.Shared.Attributes
+{
+    public abstract class AdditionalFilesAttribute : Attribute
+    {
+        private string[] files;
+
+        public AdditionalFilesAttribute(params string[] files)
+        {
+            this.files = files;
+        }
+
+        public IReadOnlyList<string> Files { get { return files; } }
+    }
+
+    public class XamlFilesAttribute : AdditionalFilesAttribute { }
+    public class AndroidLayoutAttribute : AdditionalFilesAttribute { }
+    public class ClassFileAttribute : AdditionalFilesAttribute { }
+}
---
+++ src/ArcGISRuntime.Samples.Shared/Attributes/SampleAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcGISRuntime.Samples.Shared.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    class SampleAttribute : Attribute
+    {
+        private string name;
+        private string description;
+        private string instructions;
+        private string[] tags;
+
+        public SampleAttribute(string name, string description, string instructions, params string[] tags)
+        {
+            this.name = name;
+            this.description = description;
+            this.instructions = instructions;
+            this.tags = tags;
+        }
+
+        public string Name { get { return name; } }
+        public string Description { get { return description; } }
+        public string Instructions { get { return instructions; } }
+        public IReadOnlyList<string> Tags { get { return tags; } }
+    }
+}
