76f9ed658f9a49cc66b0216475dea9c95f575d85
Nathan Castle <ncastle@esri.com>
2018-02-07 10:58:38 -0800

    Changes from upgrade

--- src/UWP/ArcGISRuntime.UWP.Viewer/App.xaml.cs
+++ src/UWP/ArcGISRuntime.UWP.Viewer/App.xaml.cs
@@ -18,6 +18,7 @@ using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using UnhandledExceptionEventArgs = Windows.UI.Xaml.UnhandledExceptionEventArgs;
 
 namespace ArcGISRuntime.UWP.Viewer
 {
